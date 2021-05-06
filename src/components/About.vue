de954f7d5795b6bf100f78e33af6a8a81c636282
oguzhan.kahyaoglu <oguzhan.kahyaoglu@enerjisauretim.com>
2021-05-06 18:01:05 +0300

    publish

--- src/components/About.vue
+++ src/components/About.vue
@@ -7,7 +7,8 @@
       </h1>
       <div class="subheading mb-5">
         8+ yrs. Software Arhcitect, Full-stack Developer; ISTANBUL/TURKEY
-        <a href="mailto:oguzhan.kahyaoglu@yahoo.com">oguzhan.kahyaoglu@yahoo.com</a>
+        <a href="mailto:oguzhan.kahyaoglu@yahoo.com">oguzhan.kahyaoglu@yahoo.com</a><br>
+        <a href="/cv/Oguzhan-Kahyaoglu-2021-en.pdf" target="_blank">Download CV PDF</a>
       </div>
       <p class="lead mb-5">
         Tech Influencer! Loves to spread dev-related info;<br>
